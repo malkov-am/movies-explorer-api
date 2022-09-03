410aaf4261b22b88a6e25e0685019cf52b864941
Andrei Malkov <thewildcat@yandex.ru>
2022-09-03 22:12:58 +0300

    Add option versionKey: false to schemes

--- models/user.js
+++ models/user.js
@@ -3,30 +3,33 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator(email) {
-        return validator.isEmail(email);
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator(email) {
+          return validator.isEmail(email);
+        },
+        message: ({ value }) => `${value} некорректный адрес электронной почты`,
       },
-      message: ({ value }) => `${value} некорректный адрес электронной почты`,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: [2, 'Поле "Имя" должно быть не короче 2 символов'],
+      maxlength: [30, 'Поле "Имя" должно быть не длинее 30 символов'],
     },
   },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  name: {
-    type: String,
-    required: true,
-    minlength: [2, 'Поле "Имя" должно быть не короче 2 символов'],
-    maxlength: [30, 'Поле "Имя" должно быть не длинее 30 символов'],
-  },
-});
+  { versionKey: false },
+);
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
   return this.findOne({ email })
