2fd99491f08530af73e9babaad344a31d1176b78
Andrei Malkov <thewildcat@yandex.ru>
2022-09-03 16:53:45 +0300

    Add User Schema

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 const { PORT = 3000 } = process.env;
 
+// Middlewares
+
+// Логгер запросов
+
+// Маршруты, не требующие аутентификации
+
+// Защищенные маршруты
+
+// Неправильный URL
+
+// Логгер ошибок
+
+// Обработчик ошибок celebrate
+
+// Централизованный обработчик ошибок
+app.use(errorHandler);
+
+// Запуск сервера
 app.listen(PORT);
 console.log(`Server is started at port: ${PORT}`);
---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: ({ value }) => `${value} некорректный адрес электронной почты`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Поле "Пароль" должно быть не короче 6 символов'],
+    maxlength: [30, 'Поле "Пароль" должно быть не длинее 30 символов'],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Поле "Имя" должно быть не короче 2 символов'],
+    maxlength: [30, 'Поле "Имя" должно быть не длинее 30 символов'],
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError({ message: 'Неправильные email или пароль' });
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          throw new UnauthorizedError({ message: 'Неправильные email или пароль' });
+        }
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
