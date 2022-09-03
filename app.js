b2b0df7955be2ea2156d293edec820af0b825e04
Andrei Malkov <thewildcat@yandex.ru>
2022-09-03 14:11:56 +0300

    Init ptoject & install dependencies

---
+++ app.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT);
+console.log(`Server is started at port: ${PORT}`);
